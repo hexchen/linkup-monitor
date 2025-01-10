f53c81361040ed5b54a29bd976475528d88a7216
Martin Stiehr <martin.stiehr.extern@bdr.de>
2025-01-10 10:33:12 +0100

    Fix logging

--- main.py
+++ main.py
@@ -5,11 +5,17 @@ import time
 from prometheus_client import start_http_server, Gauge
 import sys
 import yaml
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="{asctime} - {levelname} - {message}",
+    style="{",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
 
 glucoseGauge = Gauge('glucose_measurement', 'glucose measurement in mmol/L')
-
-# todo containerize
-# todo deploy to raspi4
 
 if __name__ == '__main__':
     # Start up the server to expose the prometheus metrics
@@ -21,16 +27,16 @@ if __name__ == '__main__':
     llu = linkup(config)
     llu.fetch_and_set_token()
     
-    print("get patient id")
+    logging.debug("get patient id")
     patient_id = llu.get_patient_id()
 
-    print("fetch GCM data")
+    logging.info("fetch GCM data")
     try:
         while True:
             gm = llu.get_gcm_data(patient_id)
             glucoseGauge.set(gm['Value'])
-            print(f"Value: {gm['Value']} {gm['Unit']} ({gm['Timestamp']})")
+            logging.info(f"Value: {gm['Value']} {gm['Unit']} ({gm['Timestamp']})")
             time.sleep(60)
     except KeyboardInterrupt:
-        print("\nbye")
+        logging.debug("\nexit")
         sys.exit(0)
